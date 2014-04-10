2e289caa9c340b94db696bfb7b301c5c567b3f35
Konstantin Käfer <mail@kkaefer.com>
2014-04-10 18:37:02 -0400

    correct path

--- keys/create.sh
+++ keys/create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "NOTE: Specify * as Common Name"
-openssl genrsa -out private.key 4096
-openssl req -new -x509 -nodes -sha1 -days 3650 -key private.key > public.cer
+openssl genrsa -out keys/private.key 4096
+openssl req -new -x509 -nodes -sha1 -days 3650 -key keys/private.key > keys/public.cer
